9c7277d7f51452619911de5c6f4311f96d244bd6
Matheus Mesquita <matheusmeskitah@gmail.com>
2025-04-17 16:21:28 -0300

    feat: Configure Ktor HTTP client for iOS with timeouts, content negotiation, and default request settings
    This commit enhances the Ktor HTTP client configuration for iOS by adding timeouts, content negotiation, and default request settings.
    
    -   **Timeouts:** Added the `HttpTimeout` plugin to set socket and request timeouts to 60 seconds, improving resilience against slow connections.
    -   **Content Negotiation:** Integrated the `ContentNegotiation` plugin to handle JSON serialization and deserialization, using `kotlinx.serialization` with custom settings (pretty printing, lenient parsing, ignoring unknown keys, and handling explicit nulls).
    -   **Default Request:**
        -   Configured `defaultRequest` to set the `Content-Type` header to `application/json`.
        -   Added a default URL to the `url` function.
    - The `Darwin` client implementation now implements this changes.

--- composeApp/src/iosMain/kotlin/com/meskitah/data/HttpClient.ios.kt
+++ composeApp/src/iosMain/kotlin/com/meskitah/data/HttpClient.ios.kt
@@ -2,8 +2,33 @@ package com.meskitah.data
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 actual val client: HttpClient
     get() = HttpClient(Darwin) {
+        //Timeout plugin to set up timeout milliseconds for client
+        install(HttpTimeout) {
+            socketTimeoutMillis = 60_000
+            requestTimeoutMillis = 60_000
+        }
 
+        defaultRequest {
+            header("Content-Type", "application/json")
+            url("https://5b94bbb0-4b84-4173-8753-c9b46c84fc76.mock.pstmn.io/appointment_availabilities/")
+        }
+
+        //ContentNegotiation plugin for negotiation media types between the client and server
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+                explicitNulls = false
+            })
+        }
     }
